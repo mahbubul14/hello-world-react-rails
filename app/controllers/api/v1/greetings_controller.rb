676403fa7c44adc8d0715c4f4cbc579d810f73d7
mahbubul14 <mahbub21alam2017@gmail.com>
2022-05-07 04:18:32 +0600

    Remove commented code

--- app/controllers/api/v1/greetings_controller.rb
+++ app/controllers/api/v1/greetings_controller.rb
@@ -1,7 +1,6 @@
 class Api::V1::GreetingsController < ApplicationController
   def index
     @greetings = Greeting.all
-    # render json: @greetings, status: :ok
 
     render json: @greetings.sample, status: :ok
   end
